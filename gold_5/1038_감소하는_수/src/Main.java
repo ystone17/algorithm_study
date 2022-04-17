e57d6006d826c95de60ac558101de9932e674405
ystone17 <ston5519@gmail.com>
2022-04-17 20:18:58 +0900

    gold 5 1038 감소하는 수 bitmask 활용 풀이

--- gold_5/1038_감소하는_수/src/Main.java
+++ gold_5/1038_감소하는_수/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
@@ -11,10 +12,23 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
 
+        long[] aaa = new long[1023];
+        for (int i = 1; i <= 1023; i++) {
+            long num = 0;
+            int temp = i;
+            for (int idx = 9; idx >= 0; idx--) {
+                if(temp % 2 == 1) num = num * 10 + idx;
+                temp /= 2;
+            }
+            aaa[i - 1] = num;
+        }
+        Arrays.sort(aaa);
+
         if(n > 1022) System.out.println(-1);
         else{
-            solve(10,10, "");
-            System.out.println(answer);
+            System.out.println(aaa[n]);
+//            solve(10,10, "");
+//            System.out.println(answer);
         }
 
 
