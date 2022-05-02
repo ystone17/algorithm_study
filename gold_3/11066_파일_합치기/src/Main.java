8cbe407d437c72a444b5ce39cbcaa75efccb68d5
ystone17 <ston5519@gmail.com>
2022-05-02 22:25:02 +0900

    2022-05-02
    * x 11066_파일_합치기 : 2차원 dp 일때 마저 선형적으로 사고하여 이해의
      방해가 있었음. + 초기값과 점화식에 대한 정확한 정의가 필요함

--- gold_3/11066_파일_합치기/src/Main.java
+++ gold_3/11066_파일_합치기/src/Main.java
@@ -6,13 +6,11 @@ import java.util.*;
 public class Main {
 
     static int tc, n;
-    static long res;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
-    static List<Long> list = new LinkedList<>();
-    static long[] file;
-    static long[][] dp;
+    static int[] sum;
+    static int[][] dp;
 
     public static void main(String[] args) throws IOException {
         tc = Integer.parseInt(br.readLine());
@@ -20,19 +18,27 @@ public class Main {
         while (tc-- > 0) {
 
             n = Integer.parseInt(br.readLine());
-            file = new long[n];
-            dp = new long[n][n];
+            sum = new int[n + 1];
+            dp = new int[n + 1][n + 1];
 
             st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < n; i++) {
-                long e = Integer.parseInt(st.nextToken());
-                file[i] = e;
-                dp[i][i] = e;
+            for (int i = 1; i < n + 1; i++) {
+                int size = Integer.parseInt(st.nextToken());
+                sum[i] = sum[i - 1] + size;
             }
 
 
-
-
+            for (int k = 2; k <= n; k++) {
+                for (int from = 1; from < n && from + k - 1 <= n; from++) {
+                    int to = from + k - 1;
+                    dp[from][to] = Integer.MAX_VALUE;
+                    for (int m = from; m < to; m++) {
+                        dp[from][to] = Math.min(dp[from][to], dp[from][m] + dp[m + 1][to]);
+                    }
+                    dp[from][to] += sum[to] - sum[from - 1];
+                }
+            }
+            sb.append(dp[1][n]).append("\n");
         }
         System.out.println(sb);
     }
