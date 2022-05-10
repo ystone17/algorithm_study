3a04a5cd63a2fb853db11656f46702303d0e38ec
ystone17 <ston5519@gmail.com>
2022-05-11 01:46:05 +0900

    2022-05-11
    * o 11437_LCA : LCA 복습
    * o 2805_나무_자르기 : 다시 커밋, 이분 탐색으로 해결

--- gold_3/11437_LCA/src/Main.java
+++ gold_3/11437_LCA/src/Main.java
@@ -1,2 +1,94 @@
-package PACKAGE_NAME;public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static int v, tc, maxAncestor;
+    static List<List<Integer>> graph = new ArrayList<>();
+    static int[][] ancestor;
+    static int[] depths;
+
+    public static void main(String[] args) throws IOException {
+        v = Integer.parseInt(br.readLine());
+
+        maxAncestor = (int) (Math.log(v) / Math.log(2)) + 1;
+        ancestor = new int[maxAncestor][v + 1];
+        depths = new int[v + 1];
+        for (int i = 0; i < v + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < v - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        setAncestor(0, 1);
+
+        tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            sb.append(LCA(u, v)).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+
+    static void setAncestor(int parent, int child) {
+        depths[child] = depths[parent] + 1;
+        ancestor[0][child] = parent;
+
+        for (int i = 1; i < maxAncestor; i++) {
+            ancestor[i][child] = ancestor[i - 1][ancestor[i - 1][child]];
+        }
+
+        for (Integer c : graph.get(child)) {
+            if (parent == c) continue;
+            setAncestor(child, c);
+        }
+    }
+
+
+    static int LCA(int u, int v) {
+        if (depths[u] < depths[v]) {
+            int temp = u;
+            u = v;
+            v = temp;
+        }
+
+        int binary = depths[u] - depths[v];
+        int pos = 0;
+        while (binary != 0){
+            if((binary & 1) == 1){
+                u = ancestor[pos][u];
+            }
+            binary >>= 1;
+            pos++;
+        }
+
+        if (u == v) return u;
+
+        for (int i = maxAncestor - 1; i >= 0; i--) {
+            if (ancestor[i][u] != ancestor[i][v]) {
+                u = ancestor[i][u];
+                v = ancestor[i][v];
+            }
+        }
+
+        return ancestor[0][u];
+    }
+
 }
--- silver_2/2805_나무_자르기/src/Main.java
+++ silver_2/2805_나무_자르기/src/Main.java
@@ -1,2 +1,59 @@
-package PACKAGE_NAME;public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int n, goal, maxLen;
+    static int[] trees;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        goal = Integer.parseInt(st.nextToken());
+
+        trees = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            trees[i] = Integer.parseInt(st.nextToken());
+            maxLen = Math.max(maxLen, trees[i]);
+        }
+
+        int binary = binary();
+        System.out.println(binary - 1);
+
+    }
+
+    static int binary() {
+        int left = 0;
+        int right = maxLen;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (calcLen(mid)) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+    static boolean calcLen(int len) {
+        long res = 0;
+        for (int tree : trees) {
+            if (tree <= len) continue;
+            res += tree - len;
+            if(res >= goal) return true;
+        }
+
+        return false;
+    }
+
+
 }
