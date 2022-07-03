e5c4054d69e84abbabb120428408c47e3aa21c1f
ystone17 <ston5519@gmail.com>
2022-07-03 09:03:09 +0900

    2022-07-03
    o s3 1904_01타일: dp, 1 타일이 붙을 때와, 00 타일이 불을 때 경우의 수를
    합쳐주었다.

---
+++ silver_3/1904_01타일/src/Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int[] dp = new int[1000001];
+
+    public static void main(String[] args) throws IOException {
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i < dp.length; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+        }
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(dp[n]);
+
+    }
+}
