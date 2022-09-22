826a7c8fde249d1cc67c12f7fcdc47ba3024fc88
ystone17 <ston5519@gmail.com>
2022-09-22 22:27:32 +0900

    2022-09-22
    * o s5 11004_K번째_수

---
+++ silver_5/11004_K번째_수/src/Main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int n, k;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        System.out.println(arr[k - 1]);
+    }
+}
