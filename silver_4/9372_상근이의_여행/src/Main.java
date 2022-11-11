c783df06e154f4bb47a534a432fca4e1c1624039
ystone17 <ston5519@gmail.com>
2022-11-11 17:08:15 +0900

    2022-11-11
    * o s4 9372_상근이의_여행: 넌센스
    
    한 경로에 하나의 도시만 갈 수 있기 때문에 모든 도시를 들리려면 비행기를
    n-1 번 타야한다.

---
+++ silver_4/9372_상근이의_여행/src/Main.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int tc;
+    static int n, m;
+
+    public static void main(String[] args) throws IOException {
+        tc = Integer.parseInt(br.readLine());
+
+        while (tc-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            for (int i = 0; i < m; i++) br.readLine();
+            sb.append(n - 1).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
