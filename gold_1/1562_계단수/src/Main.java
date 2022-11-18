65db5fa433c02dc0a0e095daaa2eb1bd0b1a9bd6
ystone17 <ston5519@gmail.com>
2022-11-19 01:12:36 +0900

    2022-11-19
    * o g1 1562_계단수: dp
    
    어제 못 푼 문제 이어서 해결
    
    어떤 n개의 수가 있을때 어떤 숫자로 끝나고 어떤 숫자를 가지고 있는지를 안다면
    
    n+1 개의 수 중 어떤 숫자보다 +1 or -1 인 수가 몇개인지 알 수 있다. 이를 점화식으로 표현하여 dp를 활용하여 문제 해결

--- gold_1/1562_계단수/src/Main.java
+++ gold_1/1562_계단수/src/Main.java
@@ -10,51 +10,43 @@ public class Main {
     static StringTokenizer st;
 
     static int n;
-    static long[][][] dp = new long[100][10][10];
+    static int[][][] dp = new int[101][10][1 << 10];
     static final int max = 1000000000;
 
     public static void main(String[] args) throws IOException {
         n = Integer.parseInt(br.readLine());
 
-        dp[10][9][0] = 1;
-        dp[10][0][9] = 1;
 
-        for (int len = 11; len <= n; len++) {
-            for (int start = 0; start < 10; start++) {
-                for (int end = 0; end < 10; end++) {
-                    if (start - 1 >= 0) {
-                        dp[len][start][end] += dp[len - 1][start - 1][end];
-                        dp[len][start][end] %= max;
+        for (int i = 1; i < 10; i++) {
+            dp[1][i][1 << i] = 1;
+        }
+
+        for (int len = 1; len < 100; len++) {
+            for (int endNum = 0; endNum < 10; endNum++) {
+                for (int bit = 0; bit < (1 << 10); bit++) {
+                    if (dp[len][endNum][bit] == 0) continue;
+                    if (endNum + 1 < 10) {
+                        dp[len + 1][endNum + 1][add(bit, endNum + 1)] += dp[len][endNum][bit];
+                        dp[len + 1][endNum + 1][add(bit, endNum + 1)] %= max;
                     }
-
-                    if (start + 1 < 10) {
-                        dp[len][start][end] += dp[len - 1][start + 1][end];
-                        dp[len][start][end] %= max;
-                    }
-
-                    if (end - 1 >= 0) {
-                        dp[len][start][end] += dp[len - 1][start][end - 1];
-                        dp[len][start][end] %= max;
-                    }
-
-                    if (end + 1 < 10) {
-                        dp[len][start][end] += dp[len - 1][start][end + 1];
-                        dp[len][start][end] %= max;
+                    if (endNum - 1 >= 0) {
+                        dp[len + 1][endNum - 1][add(bit, endNum - 1)] += dp[len][endNum][bit];
+                        dp[len + 1][endNum - 1][add(bit, endNum - 1)] %= max;
                     }
                 }
             }
         }
 
-        long res = 0;
-        for (int len = 10; len <= n; len++) {
-            for (int start = 1; start < 10; start++) {
-                for (int end = 0; end < 10; end++) {
-                    res += dp[len][start][end];
-                    res %= max;
-                }
-            }
+        int res = 0;
+        for (int endNum = 0; endNum < 10; endNum++) {
+            res += dp[n][endNum][(1 << 10) - 1];
+            res %= max;
         }
 
         System.out.println(res);
+    }
+
+    static int add(int origin, int target) {
+        return origin | (1 << target);
     }
 }
