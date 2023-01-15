dbb52f7a63f771d55c6c923f73d55a273bf6f590
ystone17 <ston5519@gmail.com>
2023-01-16 00:59:35 +0900

    2023-01-16
    * o s3 18310_안테나: 구현
    
    한 집 씩 비교하면서 기준이 되는 집에 왼쪽에 있는 집 개수만큼 더해주고 오른쪽에 있는 집 개수만큼 빼준다. 그럼 각 집에서의 총 거리가 나온다.

---
+++ silver_3/18310_안테나/src/Main.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int n;
+    static int[] home = new int[200_001];
+    static int homeCnt, idxBefore, resultIdx;
+    static long minDist, resultMax = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int h = Integer.parseInt(st.nextToken());
+            home[h]++;
+            minDist += h;
+        }
+
+        for (int i = 1; i < home.length; i++) {
+            if (home[i] == 0) continue;
+            long dist = i - idxBefore;
+
+            minDist = minDist - (dist * (n - homeCnt)) + (dist * homeCnt);
+            homeCnt += home[i];
+            idxBefore = i;
+
+            if (resultMax > minDist) {
+                resultMax = minDist;
+                resultIdx = i;
+            }
+        }
+
+        System.out.println(resultIdx);
+    }
+}
