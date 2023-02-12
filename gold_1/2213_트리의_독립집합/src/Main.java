5a84296f903458d17e4da69ecca8c9cd6c45d8fa
ystone17 <ston5519@gmail.com>
2023-02-13 02:28:03 +0900

    2023-02-13
    * o g1 2213_트리의_독립집합: tree, 재귀
    
    이전에 재귀로 풀었던 트리 문제와 유사해보임. 다시 풀어볼 예정~
    
    ==
    
    dfs 진행하면서 포함하는 노드를 기록하는게 어려웠던 문제.
    
    솔직하게 이렇게 알고리즘이 진행 될 때 used[] 배열이 신뢰할 수 있는 배열이 되는지 잘 모르겠다.
    
    결론적으로 최대값을 구하면서 경로를 구할 경우 잦은 리스트 생성으로 인한 메모리 초과가 발생한다.
    
    그래서 최대값을 구하면서 기록만 해놓은뒤 다시 한번 dfs를 통해 경로를 구한다.
    
    다시 한 번 검토를 해봐야 할 거 같은 문제이다.

--- gold_1/2213_트리의_독립집합/src/Main.java
+++ gold_1/2213_트리의_독립집합/src/Main.java
@@ -2,10 +2,7 @@ import javax.xml.transform.Source;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
@@ -16,10 +13,15 @@ public class Main {
     static List<List<Integer>> tree = new ArrayList<>();
     static int n;
     static int[] g;
+    static int[] used;
+    static int[][] dp;
+    static PriorityQueue<Integer> pq = new PriorityQueue<>();
 
     public static void main(String[] args) throws IOException {
         n = Integer.parseInt(br.readLine());
         g = new int[n + 1];
+        used = new int[n + 1];
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             g[i + 1] = Integer.parseInt(st.nextToken());
@@ -37,59 +39,78 @@ public class Main {
             tree.get(u).add(v);
             tree.get(v).add(u);
         }
-        List<Integer> containRoute = new ArrayList<>();
-        int containRes = req(-1, 1, true, containRoute);
 
-        List<Integer> notContainRoute = new ArrayList<>();
-        int notContainRes = req(-1, 1, false, notContainRoute);
+        dp = new int[n + 1][2];
+        int containRes = req(-1, 1, 1);
+        dp = new int[n + 1][2];
+        int notContainRes = req(-1, 1, 0);
 
 
         if(containRes > notContainRes) {
-            Collections.sort(containRoute);
-            sb.append(containRes).append("\n");
-            for (Integer i : containRoute) {
-                sb.append(i).append(" ");
-            }
-        } else {
-            Collections.sort(notContainRoute);
-            sb.append(containRes).append("\n");
-            for (Integer i : notContainRoute) {
-                sb.append(i).append(" ");
-            }
+            used[1] = 1;
+        }
+
+        sb.append(Math.max(containRes, notContainRes)).append("\n");
+
+        trace(1, 0, used[1]);
+
+        while (!pq.isEmpty()) {
+            sb.append(pq.poll()).append(" ");
         }
 
         System.out.println(sb);
     }
 
-    static int req(int parent, int me, boolean isContain, List<Integer> route) {
-        int res = isContain ? g[me] : 0;
-        if (isContain) route.add(me);
 
-        for (Integer child : tree.get(me)) {
-            if (child == parent) continue;
-
-            List<Integer> notContainRoute = new ArrayList<>();
-            int subRes = req(me, child, false, notContainRoute);
-
-
-            if (!isContain) {
-                List<Integer> containRoute = new ArrayList<>();
-                int subContainRes = req(me, child, true, containRoute);
-
-                if (subContainRes >= subRes) {
-                    subRes = subContainRes;
-                    route.addAll(containRoute);
-                } else {
-                    route.addAll(notContainRoute);
-                }
-            } else {
-                route.addAll(notContainRoute);
-            }
-
-            res += subRes;
+    static int req(int parent, int me, int isContain) {
+        if(dp[me][isContain] != 0) {
+            return dp[me][isContain];
         }
 
-        return res;
+        int res = 0;
+
+        if (isContain == 1) {
+            for (Integer child : tree.get(me)) {
+                if (child == parent) continue;
+
+                int subRes = req(me, child, 0);
+
+                res += subRes;
+            }
+            return res + g[me];
+        } else {
+            for (Integer child : tree.get(me)) {
+                if (child == parent) continue;
+
+                dp[child][1] = req(me, child, 1);
+                dp[child][0] = req(me, child, 0);
+
+                if(dp[child][1] > dp[child][0]) {
+                    used[child] = 1;
+                } else {
+                    used[child] = 0;
+                }
+
+
+                res += Math.max(dp[child][1], dp[child][0]);
+            }
+            return res;
+        }
+    }
+
+    static void trace(int cur, int parent, int contain) {
+        if(contain == 1) {
+            pq.add(cur);
+            for (Integer next : tree.get(cur)) {
+                if(next == parent) continue;
+                trace(next, cur, 0);
+            }
+        } else {
+            for (Integer next : tree.get(cur)) {
+                if(next == parent) continue;
+                trace(next, cur, used[next]);
+            }
+        }
     }
 
 }
