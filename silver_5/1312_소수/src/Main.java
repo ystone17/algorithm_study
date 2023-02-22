aa4d1a9db408cc4c67ec5dd21bc974fd7f69501d
ystone17 <ston5519@gmail.com>
2023-02-22 21:32:59 +0900

    2023-02-22
    * o s5 1312_소수: 브루트 포스

---
+++ silver_5/1312_소수/src/Main.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int a, b, c, res;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i <= c; i++) {
+            res = a / b;
+            a = a % b * 10;
+        }
+
+        System.out.println(res);
+
+    }
+}
