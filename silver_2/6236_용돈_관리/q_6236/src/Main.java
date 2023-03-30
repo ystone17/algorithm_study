5a19d113cd9d60ecdb3c1aeed884018ee695cb8a
ystone17 <ston5519@gmail.com>
2023-03-30 20:38:15 +0900

    2023-03-30
    * x s2 6236_용돈_관리: 이분탐색
    
    다시 풀 예정

---
+++ silver_2/6236_용돈_관리/q_6236/src/Main.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int n, m;
+    static int[] cost;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        cost = new int[n];
+        for (int i = 0; i < n; i++) {
+            cost[i] = Integer.parseInt(br.readLine());
+        }
+
+        int left = 0;
+        for (int i : cost) {
+            left = Math.max(left, i);
+        }
+        int right = Integer.MAX_VALUE;
+        int mid;
+
+        while (left < right){
+            mid = (left  + right) / 2;
+
+            if(ok(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        System.out.println(left);
+
+
+    }
+
+    private static boolean ok(int k) {
+        int mCnt = 0;
+        int wallet = 0;
+        for (int i = 0; i < n; i++) {
+            if (wallet < cost[i]) {
+                wallet = k;
+                mCnt++;
+            }
+            wallet -= cost[i];
+            if(mCnt > m) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
