567f737bc233ef6008d41f0b28a5c6426e0bf3be
ystone17 <ston5519@gmail.com>
2023-05-13 11:11:41 +0900

    파일 위치 변경

--- bronze/1934_최소공배수/src/Main.java
+++ bronze/1934_최소공배수/src/Main.java
@@ -6,40 +6,40 @@ import java.util.StringTokenizer;
 
 public class Main{
 
-     public static void main(String []args) throws IOException{
+    public static void main(String []args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        
+
         int t = Integer.parseInt(br.readLine());
         while(t-- > 0) {
             st = new StringTokenizer(br.readLine());
 
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            
+
             System.out.println(lcm(a, b, gcd(a, b)));
         }
-        
-     }
-     
+
+    }
+
     static int lcm(int a, int b, int gcd){
         return a * b / gcd;
     }
-     
-     static int gcd(int a, int b) {
-         if( a < b) {
-             int temp = a;
-             a = b;
-             b = temp;
-         }
-         
-         
-         while(b != 0) {
-             int r = a % b;
-             a = b;
-             b = r;
-         }
-         
-         return a;
-     }
+
+    static int gcd(int a, int b) {
+        if( a < b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+
+        while(b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+
+        return a;
+    }
 }
