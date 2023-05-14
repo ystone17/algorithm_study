87b824af91103edb495082e616be940c10f8e965
ystone17 <ston5519@gmail.com>
2023-05-14 20:42:13 +0900

    2023-05-14
    * o b1 2163_초콜릿_자르기: 구현

---
+++ bronze/2163_초콜릿_자르기/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int n, m;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        System.out.println((n - 1) + (n * (m - 1)));
+    }
+}
