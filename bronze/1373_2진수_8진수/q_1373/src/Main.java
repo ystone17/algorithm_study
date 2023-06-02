531ccfed49f6f4788f524b73117b3a264173aa9e
ystone17 <ston5519@gmail.com>
2023-06-03 02:31:58 +0900

    2023-06-03
    * o b1 1373_2진수_8진수: 구현
    
    2진수로 주어지는 값의 최대 크기가 100,000 이기 때문에 int 로 변환시 NumberFormat exception 발생
    
    2진수 3자리는 8진수 1자리와 동일하기때문에 3자리씩 나누어서 변환해준다.

--- bronze/1373_2진수_8진수/q_1373/src/Main.java
+++ bronze/1373_2진수_8진수/q_1373/src/Main.java
@@ -1,12 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
-        System.out.println(Integer.toOctalString(Integer.parseInt(br.readLine(), 2)));
+        String binaryStr = br.readLine();
+
+        for (int i = binaryStr.length(); i - 3 >= 0; i -= 3) {
+            String substring = binaryStr.substring(i - 3, i);
+            sb.append(Integer.parseInt(substring, 2));
+        }
+
+        if (binaryStr.length() % 3 != 0) {
+            String substring = binaryStr.substring(0, binaryStr.length() % 3);
+            sb.append(Integer.parseInt(substring, 2));
+        }
+
+        sb.reverse();
+        System.out.println(sb);
+
     }
 }
