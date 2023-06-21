4517b13b6ad12030d662b4d31e1919e89017cab1
ystone17 <ston5519@gmail.com>
2023-06-22 01:59:18 +0900

    2023-06-22
    * o s5 2669_직사각형_네개의_합집합의_면적_구하기: 구현
    
    주어진 좌표대로 직접 도화지에 그리듯이 그린 후 계산

---
+++ silver_5/2669_직사각형_네개의_합집합의_면적_구하기/src/Main.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int[][] map = new int[200][200];
+    static int res;
+
+    public static void main(String[] args) throws IOException {
+        for (int i = 0; i < 4; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            for (int y = y1; y < y2; y++) {
+                for (int x = x1; x < x2; x++) {
+                    map[y][x] = 1;
+                }
+            }
+        }
+
+        for (int[] row : map) {
+            for (int col : row) {
+                if(col == 1) {
+                    res++;
+                }
+            }
+        }
+
+        System.out.println(res);
+    }
+}
