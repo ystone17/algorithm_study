bb2c0e45373485b63833842d3b7024a52ecdea4b
ystone17 <ston5519@gmail.com>
2023-07-20 02:06:02 +0900

    2023-07-20
    * o b4 5717_상근이의_친구들:

---
+++ bronze/5717_상근이의_친구들/src/Main.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if(a == 0 && b == 0) {
+                break;
+            }
+
+            sb.append(a + b).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
