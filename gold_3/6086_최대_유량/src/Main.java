15387b5ec1594a05f150466add77f16b24e4bf66
ystone17 <ston5519@gmail.com>
2023-07-22 23:33:49 +0900

    2023-07-22
    * o 6086_최대_유량: 네트워크 유량(network flow), 에드몬드 카프
    
    network flow, 최대 유량, 등등의 유형으로 불리는 문제. 범용적인 알고리즘은 에드몬드 카프와 포드 풀커슨이 있다. 에드몬드 카프 방식으로 문제 해결
    
    에드몬드 카프는 음의 유량의 개념을 이용하여 풀이한 알고리즘이다.
    
    어떤 경로를 찾았을때 반대 방향으로 같은 양의 음의 유량을 설정해준다. 새로 찾은 경로가 음의 유량이 설정된 간선을 지날 수 있다면, 그만큼의 양을 뺏어와 새로운 경로를 이어갈 수 있다. 기존 경로 또한 새로운 경로에서 받은 유량으로 채워진다.
    
    bfs 를 통해 하나의 경로를 찾고, 최소 유량을 찾은 뒤 양의 유량 음의 유량을 더해준다. 경로를 못 찾을때까지 반복한다.

--- gold_3/6086_최대_유량/src/Main.java
+++ gold_3/6086_최대_유량/src/Main.java
@@ -1,8 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,56 +12,68 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
 
-    static List<List<Integer>> graph = new ArrayList<>();
-    static int[][] flowRates = new int[52][52];
-    static int[] visited = new int[52];
+    static final int A = 'A' - 65;
+    static final int Z = 'Z' - 65;
+
+    static Queue<Integer> q = new LinkedList<>();
+    static int result;
+    static int[][] capacities = new int[52][52];
+    static int[][] flows = new int[52][52];
+    static int[] route = new int[52];
 
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < 52; i++) {
-            graph.add(new ArrayList<>());
-        }
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int u = calcNodeNum(st.nextToken());
             int v = calcNodeNum(st.nextToken());
-            int flowRate = Integer.parseInt(st.nextToken());
+            int capacity = Integer.parseInt(st.nextToken());
 
-            graph.get(u).add(v);
-            flowRates[u][v] = flowRate;
+            capacities[u][v] += capacity;
+            capacities[v][u] += capacity;
         }
 
-        visited[0] = 1;
-        System.out.println(calcTotalFlowRate(-1, 0));
+        while (true) {
+            Arrays.fill(route, -1);
+            q.clear();
+
+            route[A] = A;
+            q.add(A);
+
+            while (!(q.isEmpty()) && route[Z] == -1) {
+                int cur = q.poll();
+
+                for (int i = 0; i < capacities[cur].length; i++) {
+                    if (capacities[cur][i] == 0) continue;
+                    if (route[i] != -1) continue;
+                    if (capacities[cur][i] - flows[cur][i] <= 0) continue;
+
+                    route[i] = cur;
+                    q.add(i);
+                }
+            }
+
+            if (route[Z] == -1) break;
+
+            int minFlow = Integer.MAX_VALUE;
+            for (int r = Z; r != A; r = route[r]) {
+                minFlow = Math.min(minFlow, capacities[route[r]][r] - flows[route[r]][r]);
+            }
+
+            for (int r = Z; r != A; r = route[r]) {
+                flows[route[r]][r] += minFlow;
+                flows[r][route[r]] -= minFlow;
+            }
+
+            result += minFlow;
+        }
+
+        System.out.println(result);
     }
 
     private static int calcNodeNum(String s) {
         int node = s.charAt(0) - 65;
         return node < 26 ? node : node - 6;
-    }
-
-    static int calcTotalFlowRate(int parent, int me) {
-        List<Integer> children = graph.get(me);
-        if (me == 'Z' - 65) {
-            return Integer.MAX_VALUE;
-        }
-
-        if (children.isEmpty()) {
-            return 0;
-        }
-
-        int totalFlowRate = 0;
-
-        for (Integer child : children) {
-            if (child == parent) {
-                continue;
-            }
-
-            totalFlowRate += Math.min(flowRates[me][child], calcTotalFlowRate(me, child));
-        }
-
-        return totalFlowRate;
     }
 }
