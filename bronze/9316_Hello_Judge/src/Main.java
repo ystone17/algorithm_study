eb5edfc732f4ffe33add399fa29c9689061ccebf
ystone17 <ston5519@gmail.com>
2023-07-24 23:20:12 +0900

    2023-07-24
    * o b4 9316_Hello_Judge:

---
+++ bronze/9316_Hello_Judge/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("Hello World, Judge %d!\n", i + 1);
+        }
+    }
+}
