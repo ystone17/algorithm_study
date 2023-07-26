17870e230deba53368f8338b37d1318bdc80f3d0
ystone17 <ston5519@gmail.com>
2023-07-26 23:00:42 +0900

    2023-07-26
    * o b4 15439_베라의_패션:

---
+++ bronze/15439_베라의_패션/src/Main.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(n * (n - 1));
+    }
+}
