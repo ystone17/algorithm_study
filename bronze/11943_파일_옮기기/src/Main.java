e14f8b2cfb2ee120d54c0fa9f16ab1f07e116e80
ystone17 <ston5519@gmail.com>
2023-07-27 01:22:21 +0900

    2023-07-27
    * o b4 11943_파일_옮기기:

---
+++ bronze/11943_파일_옮기기/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int a, b, c, d;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        c = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
+
+        System.out.println(Math.min(a + d, b + c));
+    }
+}
