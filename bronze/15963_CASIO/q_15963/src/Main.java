8a449dee00e125056a97831c21832ed77d8ec2d7
ystone17 <ston5519@gmail.com>
2023-08-28 20:52:16 +0900

    2023-08-28
    * o b 15963_CASIO: 구현

---
+++ bronze/15963_CASIO/q_15963/src/Main.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        long n = Long.parseLong(st.nextToken());
+        long m = Long.parseLong(st.nextToken());
+
+        System.out.println(n == m ? 1 : 0);
+    }
+}
