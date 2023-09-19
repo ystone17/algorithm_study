7f930f355a9600e9d0bc8be4d7bfc94f3ad9d69d
ystone17 <ston5519@gmail.com>
2023-09-20 02:59:00 +0900

    2023-09-20
    * x g2 17825_주사위_윷놀이: 구현
    
    같은 장판을 밟을 경우 스킵해야한다.
    
    예를 들어 40은 [0][12] 인데 [2][20] 이랑 같은 자리이다. 이 부분을 확인하지 못해 오차가 발생한다.

---
+++ gold_2/17825_주사위_윷놀이/src/Main.java
@@ -0,0 +1,94 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int[] knightSeq = new int[10];
+    static int[][] scoreMap = {
+            {0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 0},
+            {0, 2, 4, 6, 8, 10, 13, 16, 19, 25, 30, 35, 40, 0},
+            {0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 25, 30, 35, 40, 0},
+            {0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 28, 27, 26, 25, 30, 35, 40, 0}
+    };
+    static int[][] map;
+
+    static int[] dice = new int[10];
+    static int res;
+    static List<Knight> knights = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        for (int i = 0; i < 5; i++) {
+            knights.add(new Knight());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 10; i++) {
+            dice[i] = Integer.parseInt(st.nextToken());
+        }
+
+        req(0);
+
+        System.out.println(res);
+    }
+
+    static void req(int cnt) {
+        if (cnt >= 10) {
+            knightSeq = new int[]{0, 0, 1, 0, 2, 2, 2, 0, 2, 2};
+            int max = 0;
+            knights.forEach(Knight::init);
+            map = new int[4][30];
+            for (int[] route : map) {
+                Arrays.fill(route, -1);
+            }
+
+            for (int i = 0; i < 10; i++) {
+                Knight knight = knights.get(knightSeq[i]);
+
+                int nRouteNum = knight.routeNum == 0 && (knight.num == 5 || knight.num == 10 || knight.num == 15) ? knight.routeNum + 1 : knight.routeNum;
+                int nNum = Math.min(knight.num + dice[i], scoreMap[nRouteNum].length - 1);
+
+                if (scoreMap[nRouteNum].length != nNum + 1 &&  map[nRouteNum][nNum] != -1) {
+                    return;
+                }
+
+                map[knight.routeNum][knight.num] = -1;
+                map[nRouteNum][nNum] = knightSeq[i];
+                knight.routeNum = nRouteNum;
+                knight.num = nNum;
+
+
+                max += scoreMap[knight.routeNum][knight.num];
+            }
+
+            res = Math.max(res, max);
+            if(max == 218) {
+                System.out.println(Arrays.toString(knightSeq));
+            }
+            return;
+        }
+
+        for (int i = 0; i < 5; i++) {
+            knightSeq[cnt] = i;
+            req(cnt + 1);
+        }
+    }
+
+    static class Knight {
+        int routeNum;
+        int num;
+
+        public void init() {
+            routeNum = 0;
+            num = 0;
+        }
+    }
+}
