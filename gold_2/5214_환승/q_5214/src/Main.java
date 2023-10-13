6c8fc9f07c890214109a369bfe741b5ee403e14b
ystone17 <ston5519@gmail.com>
2023-10-13 21:07:31 +0900

    2023-10-13
    * o g2 2610_회의준비: BFS
    
    하이퍼 튜브 자체를 하나의 노드로 보고 BFS 로 접근 튜브 내의 가상의 중심역을 만들어 인접그래프를 간단하게 만들 수 있다.
    
    모든 역은 튜브의 중심역을 지나야 하기 때문에 도달하는 거리가 최단거리가 된다.
    모든 역은 튜브의 중심역을 지나야 하기 때문에 어느 역을 가든 거리가 2배가 되기 때문에 절반 거리가 진짜 거리가 된다.

--- gold_2/5214_환승/q_5214/src/Main.java
+++ gold_2/5214_환승/q_5214/src/Main.java
@@ -10,8 +10,9 @@ public class Main {
     static StringTokenizer st;
 
     static int n, tubeSize, tubeMaxNum;
-    static int[][] tubes;
-    static int[] dist;
+    static List<List<Integer>> graph = new ArrayList<>();
+    static int[] dist, visited;
+    static Queue<Integer> q = new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
@@ -20,17 +21,40 @@ public class Main {
         tubeSize = Integer.parseInt(st.nextToken());
         tubeMaxNum = Integer.parseInt(st.nextToken());
 
-        dist = new int[n + 1];
-        tubes = new int[tubeMaxNum][tubeSize];
+        for (int i = 0; i < n + 1 + tubeMaxNum; i++) {
+            graph.add(new ArrayList<>());
+        }
 
         for (int tubeNum = 0; tubeNum < tubeMaxNum; tubeNum++) {
             st = new StringTokenizer(br.readLine());
 
             for (int i = 0; i < tubeSize; i++) {
-                tubes[tubeNum][i] = Integer.parseInt(st.nextToken());
+                int station = Integer.parseInt(st.nextToken());
+                graph.get(station).add(tubeNum + 1 + n);
+                graph.get(tubeNum + 1 + n).add(station);
             }
         }
 
+        dist = new int[n + 1 + tubeMaxNum];
+        visited = new int[n + 1 + tubeMaxNum];
 
+        q.add(1);
+        dist[1] = 1;
+        visited[1] = 1;
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+
+            for (int next : graph.get(cur)) {
+                if (visited[next] == 1) {
+                    continue;
+                }
+                visited[next] = 1;
+                dist[next] = dist[cur] + 1;
+                q.add(next);
+            }
+        }
+
+        System.out.println(visited[n] == 1 ? dist[n] / 2 + 1 : -1);
     }
 }
