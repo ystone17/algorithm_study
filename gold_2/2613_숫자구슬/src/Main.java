24a3b807c71fe1fc2e74e7ecaa6427d3f64ccb82
ystone17 <ston5519@gmail.com>
2023-10-26 23:01:54 +0900

    2023-10-26
    * x g2 2613_숫자구슬: dp
    
    문제에서 원하는 최대값을 구했지만, 최대값에서의 묶음별 크기를 구하지 못함.

---
+++ gold_2/2613_숫자구슬/src/Main.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int n, m;
+    static int[] seq, partSum, size;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        seq = new int[n + 1];
+        partSum = new int[n + 1];
+        size = new int[m + 1];
+        dp = new int[n + 1][m + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            seq[i + 1] = Integer.parseInt(st.nextToken());
+            partSum[i + 1] = partSum[i] + seq[i + 1];
+        }
+
+        int f = f(1, m);
+        System.out.println(f);
+    }
+
+    static int f(int inStart, int k) {
+        if (dp[inStart][k] != 0) {
+            return dp[inStart][k];
+        }
+
+        if (k == 1) {
+            return dp[inStart][1] = partSum[n] - partSum[inStart - 1];
+        }
+
+        int leftSum = 0;
+        int rightSum;
+        int diff = Integer.MAX_VALUE;
+        int max = 0;
+
+        for (int i = inStart; i <= n - k + 1; i++) {
+            leftSum += seq[i];
+            rightSum = f(i + 1, k - 1);
+
+            if (diff > Math.abs(leftSum - rightSum)) {
+                diff = Math.abs(leftSum - rightSum);
+                max = Math.max(leftSum, rightSum);
+
+            }
+
+        }
+
+        return dp[inStart][k] = max;
+    }
+
+
+}
