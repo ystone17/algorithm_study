620e2589f6da7a901c3a1c726735b11a559fb244
ystone17 <ston5519@gmail.com>
2023-10-28 21:40:07 +0900

    2023-10-28
    * x g2 2613_숫자구슬: 이분탐색

--- gold_2/2613_숫자구슬/src/Main.java
+++ gold_2/2613_숫자구슬/src/Main.java
@@ -10,9 +10,8 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
 
-    static int n, m;
-    static int[] seq, partSum, endIdxSeq;
-    static int[][] dpMax, dpEndIdx;
+    static int n, m, total;
+    static int[] seq;
 
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
@@ -20,64 +19,44 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
 
         seq = new int[n + 1];
-        partSum = new int[n + 1];
-        endIdxSeq = new int[m + 1];
-        dpMax = new int[n + 1][m + 1];
-        dpEndIdx = new int[n + 1][m + 1];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             seq[i + 1] = Integer.parseInt(st.nextToken());
-            partSum[i + 1] = partSum[i] + seq[i + 1];
+            total += seq[i + 1];
         }
 
-        int[] f = f(1, m);
-        endIdxSeq[m] = f[1];
-        endIdxSeq[1] = n;
+        int left = 1;
+        int right = total;
+        int mid;
 
-        sb.append(endIdxSeq[endIdxSeq.length - 1]).append(" ");
-        for (int i = endIdxSeq.length - 2; i >= 1; i--) {
-            sb.append(endIdxSeq[i] - endIdxSeq[i + 1]).append(" ");
-        }
+        while (left < right) {
+            mid = (left + right) / 2;
 
-        System.out.println(f[0]);
-        System.out.println(sb);
-    }
-
-    static int[] f(int end, int k) {
-        if (dpMax[end][k] != 0) {
-            return new int[]{dpMax[end][k], dpEndIdx[end][k]};
-        }
-
-        if (k == 1) {
-            return new int[]{dpMax[end][1] = partSum[n] - partSum[end - 1], n};
-        }
-
-        int leftSum = 0;
-        int rightSum;
-        int curEndIdx = 0;
-        int nextEndIdx = 0;
-        int diff = Integer.MAX_VALUE;
-        int max = 0;
-
-        for (int i = end; i <= n - k + 1; i++) {
-            leftSum += seq[i];
-            int[] f = f(i + 1, k - 1);
-            rightSum = f[0];
-
-            if (diff > Math.abs(leftSum - rightSum)) {
-                diff = Math.abs(leftSum - rightSum);
-                max = Math.max(leftSum, rightSum);
-                curEndIdx = i;
-                nextEndIdx = f[1];
+            if (possible(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
             }
-
         }
 
-        endIdxSeq[k - 1] = nextEndIdx;
-        dpEndIdx[end][k] = curEndIdx;
-        dpMax[end][k] = max;
-        return new int[]{max, curEndIdx};
+        System.out.println(left);
+
     }
+
+    private static boolean possible(int k) {
+        int count = 0;
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (sum + seq[i] > k) {
+                count++;
+                sum = 0;
+            }
+            sum += seq[i];
+        }
+
+        return count == m;
+    }
+
 
 }
