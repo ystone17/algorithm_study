18050b091b90a558818e26ba81c79e41a0959ab8
ystone17 <ston5519@gmail.com>
2023-11-09 23:50:51 +0900

    2023-11-09
    * x g2 1033_칵테일: 수학, 구현
    
    비례식을 이용한 수학 풀이를 구현했지만 실패

---
+++ gold_2/1033_칵테일/src/Main.java
@@ -0,0 +1,157 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int n;
+    static int[][] input;
+    static int[] count;
+    static List<BunSu> bunSuList = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+
+        input = new int[n][4];
+        count = new int[n];
+        for (int i = 0; i < n - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 4; j++) {
+                input[i][j] = Integer.parseInt(st.nextToken());
+            }
+            count[input[i][0]]++;
+            count[input[i][1]]++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            bunSuList.add(new BunSu());
+        }
+
+        int base = -1;
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] > 1) {
+                base = i;
+                break;
+            }
+        }
+
+        int done = 0;
+        count = new int[n];
+
+        for (int i = 0; i < n - 1; i++) {
+            int a = input[i][0];
+            int b = input[i][1];
+            int c = input[i][2];
+            int d = input[i][3];
+            if (a == base) {
+                bunSuList.get(b).bunJa = d;
+                bunSuList.get(b).bunMo = c;
+                bunSuList.get(b).counterNum = a;
+                count[b] = 1;
+                done++;
+            }
+
+            if (b == base) {
+                bunSuList.get(a).bunJa = c;
+                bunSuList.get(a).bunMo = d;
+                bunSuList.get(a).counterNum = b;
+                count[a] = 1;
+                done++;
+            }
+        }
+
+        while (done < n) {
+            for (int i = 0; i < n - 1; i++) {
+                int a = input[i][0];
+                int b = input[i][1];
+                int c = input[i][2];
+                int d = input[i][3];
+
+                if (count[a] == 0 && bunSuList.get(b).counterNum == base) {
+                    bunSuList.get(a).bunJa = bunSuList.get(b).bunJa * c;
+                    bunSuList.get(a).bunMo = bunSuList.get(b).bunMo * d;
+                    bunSuList.get(a).counterNum = base;
+                    count[a] = 1;
+                    done++;
+                    break;
+                }
+
+                if (count[b] == 0 && bunSuList.get(a).counterNum == base) {
+                    bunSuList.get(b).bunJa = bunSuList.get(a).bunJa * d;
+                    bunSuList.get(b).bunMo = bunSuList.get(a).bunMo * c;
+                    bunSuList.get(b).counterNum = base;
+                    count[b] = 1;
+                    done++;
+                    break;
+                }
+            }
+        }
+
+        for (BunSu bunSu : bunSuList) {
+            bunSu.yakBun();
+        }
+
+        int x = 1;
+
+        for (BunSu bunSu : bunSuList) {
+            x = lcm(x, bunSu.bunMo);
+        }
+
+        for (int i = 0; i < bunSuList.size(); i++) {
+            if (i == base) {
+                sb.append(x).append(" ");
+                continue;
+            }
+
+            sb.append(bunSuList.get(i).bunJa * x / bunSuList.get(i).bunMo).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+
+    static int gcd(int a, int b) {
+        if (a < b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        while (b != 0) {
+            int temp = a;
+            a = b;
+            b = temp % b;
+        }
+
+        return a;
+    }
+
+    static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+
+    static class BunSu {
+        int bunJa = 1;
+        int bunMo = 1;
+        int counterNum;
+
+        public BunSu() {
+            this.counterNum = -1;
+        }
+
+        public void yakBun() {
+            for (int i = 2; i <= 9; i++) {
+                if (bunJa % i == 0 && bunMo % i == 0) {
+                    bunJa /= i;
+                    bunMo /= i;
+                }
+            }
+        }
+    }
+}
