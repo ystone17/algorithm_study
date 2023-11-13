73d59fc186a9fe382eec63ea396e581c11fc2e47
ystone17 <ston5519@gmail.com>
2023-11-13 22:42:10 +0900

    2023-11-13
    * o s4 2870_수학숙제: 구현

--- silver_4/2870_수학숙제/src/Main.java
+++ silver_4/2870_수학숙제/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -15,13 +16,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
 
-        List<Long> list = new ArrayList<>();
+        List<BigInteger> list = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
             for (char c : br.readLine().toCharArray()) {
                 if (c - '0' < 0 || c - '0' > 9) {
                     if (sb.length() != 0) {
-                        list.add(Long.parseLong(sb.toString()));
+                        list.add(new BigInteger(sb.toString()));
                         sb = new StringBuilder();
                     }
                     continue;
@@ -30,15 +31,16 @@ public class Main {
                 sb.append(c);
             }
             if (sb.length() != 0) {
-                list.add(Long.parseLong(sb.toString()));
+                list.add(new BigInteger(sb.toString()));
                 sb = new StringBuilder();
             }
         }
 
         Collections.sort(list);
-        for (Long l : list) {
-            sb.append(l).append("\n");
+        for (BigInteger bigInteger : list) {
+            sb.append(bigInteger).append("\n");
         }
+
         System.out.println(sb);
     }
 }
