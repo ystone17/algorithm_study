dd47eb5ddea8e75df239dd487fed9471d5651577
ystone17 <ston5519@gmail.com>
2023-11-28 21:35:09 +0900

    2023-11-28
    * o g2 14476_최대공약수_하나_빼기: 누적 최대공약수
    
    주어진 수열에서 하나를 뺀다 -> 왼쪽 묶음의 최대공약수와 오른쪽 묶음의 최대공약수의 최대공약수를 구하면 된다.
    
    누적합을 활용하여 누적 최대공약수를 계산한뒤 비교하면 된다.

--- gold_2/14476_최대공약수_하나_빼기/q_14476/src/Main.java
+++ gold_2/14476_최대공약수_하나_빼기/q_14476/src/Main.java
@@ -9,49 +9,62 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
 
-    static int n;
-    static int[] seq;
+    static int n, max = -1, maxIdx, gcd;
+    static int[] seq, partGcdLtoR, partGcdRtoL;
 
     public static void main(String[] args) throws IOException {
         n = Integer.parseInt(br.readLine());
         seq = new int[n];
+        partGcdLtoR = new int[n];
+        partGcdRtoL = new int[n];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             seq[i] = Integer.parseInt(st.nextToken());
         }
 
-        int k= -1;
-        int kGcd = -1;
+        partGcdLtoR[0] = seq[0];
+        for (int i = 1; i < seq.length; i++) {
+            partGcdLtoR[i] = gcd(partGcdLtoR[i - 1], seq[i]);
+        }
 
-        for (int del = 0; del < n; del++) {
-            int gcd = del == 0 ? seq[1] : seq[0];
-            for (int i = 0; i < n; i++) {
-                if(i == del) {
-                    continue;
-                }
+        partGcdRtoL[n - 1] = seq[n - 1];
+        for (int i = seq.length - 2; i >= 0; i--) {
+            partGcdRtoL[i] = gcd(partGcdRtoL[i + 1], seq[i]);
+        }
 
-                gcd = gcd(gcd, seq[i]);
-            }
-
-            if(seq[del] % gcd == 0) {
+        for (int i = 1; i < n - 1; i++) {
+            gcd = gcd(partGcdLtoR[i - 1], partGcdRtoL[i + 1]);
+            if (seq[i] % gcd == 0) {
                 continue;
             }
 
-            if(gcd > kGcd) {
-                k = del;
-                kGcd = gcd;
+            if (max < gcd(partGcdLtoR[i - 1], partGcdRtoL[i + 1])) {
+                max = gcd;
+                maxIdx = i;
             }
         }
-        if(k == -1) {
-            System.out.println(-1);
-        } else {
-            System.out.printf("%d %d", kGcd, seq[k]);
+
+        if (max < partGcdLtoR[n - 2] && seq[n - 1] % partGcdLtoR[n - 2] != 0) {
+            max = partGcdLtoR[n - 2];
+            maxIdx = n - 1;
         }
+
+        if (max < partGcdRtoL[1] && seq[0] % partGcdRtoL[1] != 0) {
+            max = partGcdRtoL[1];
+            maxIdx = 0;
+        }
+
+        if (max == -1) {
+            System.out.print(-1);
+        } else {
+            System.out.printf("%d %d", max, seq[maxIdx]);
+        }
+
     }
 
     private static int gcd(int a, int b) {
-        if(a < b) {
+        if (a < b) {
             int temp = a;
             a = b;
             b = temp;
