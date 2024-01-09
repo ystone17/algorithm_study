7928b31f8037ff3d3d9a82f97756974f55f9461e
ystone17 <ston5519@gmail.com>
2024-01-09 22:09:46 +0900

    2024-01-09
    * o g4 1405_미친_로봇: dfs
    
    퍼센트를 double 형으로 받으면 int 형의 오버플로우를 신경쓰지 않고, 곱할 수 있게된다.
    
    dfs 로 진행하여 단순경로 가 아닌 순간 그 루트는 제외해버리면 된다.
    
    문제에선 모든 경로가 따로이기 때문에 가능하다

--- gold_4/1405_미친_로봇/src/Main.java
+++ gold_4/1405_미친_로봇/src/Main.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PrimitiveIterator;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,115 +12,49 @@ public class Main {
     static final int E = 0, W = 1, S = 2, N = 3;
 
     static int n;
-    static int[] percents = new int[4];
-    static int[] seq;
-    static int[][] map;
+    static double[] percents = new double[4];
+    static double res;
+    static int[][] map = new int[30][30];
     static int[] dy = {0, 0, 1, -1};
     static int[] dx = {1, -1, 0, 0};
-    static boolean isSimple;
-    static Fraction fraction = new Fraction();
 
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-        percents[E] = Integer.parseInt(st.nextToken());
-        percents[W] = Integer.parseInt(st.nextToken());
-        percents[S] = Integer.parseInt(st.nextToken());
-        percents[N] = Integer.parseInt(st.nextToken());
+        percents[E] = Integer.parseInt(st.nextToken()) * 0.01;
+        percents[W] = Integer.parseInt(st.nextToken()) * 0.01;
+        percents[S] = Integer.parseInt(st.nextToken()) * 0.01;
+        percents[N] = Integer.parseInt(st.nextToken()) * 0.01;
 
-        seq = new int[n];
-        req(0);
+        map[15][15] = 1;
+        dfs(15, 15, 0, 1);
 
-        System.out.printf("%.9f", (double) fraction.child / fraction.parent);
+        System.out.printf("%.10f ", res);
     }
 
-    private static void req(int idx) {
-        if (idx >= n) {
-
-            map = new int[30][30];
-
-            int y = 15;
-            int x = 15;
-            map[15][15] = 1;
-
-            Fraction f = new Fraction();
-            isSimple = true;
-
-            for (int i : seq) {
-                y += dy[i];
-                x += dx[i];
-
-                if (map[y][x] == 1) {
-                    isSimple = false;
-                }
-                map[y][x]++;
-                f.mul(percents[i], 100);
-            }
-
-            if (isSimple) {
-                fraction.add(f.child, f.parent);
-            }
-
+    private static void dfs(int y, int x, int count, double total) {
+        if (count >= n) {
+            res += total;
             return;
         }
 
         for (int i = 0; i < 4; i++) {
-            if (percents[i] == 0) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if (ny < 0 || ny >= 30 || nx < 0 || nx >= 30) {
                 continue;
             }
 
-            seq[idx] = i;
-            req(idx + 1);
+            if (map[ny][nx] == 1) {
+                continue;
+            }
+
+            map[ny][nx] = 1;
+            dfs(ny, nx, count + 1, total * percents[i]);
+            map[ny][nx] = 0;
         }
     }
 
-    private static class Fraction {
-        int child;
-        int parent;
 
-        private void add(int child, int parent) {
-            if (this.parent == 0) {
-                this.child = child;
-                this.parent = parent;
-                return;
-            }
-            this.child = this.child * parent + child * this.parent;
-            this.parent *= parent;
-
-            simplify();
-        }
-
-        private void mul(int child, int parent) {
-            if (this.parent == 0) {
-                this.child = child;
-                this.parent = parent;
-                return;
-            }
-
-            this.child *= child;
-            this.parent *= parent;
-
-            simplify();
-        }
-
-        private void simplify() {
-            int gcd = gcd();
-
-            this.child /= gcd;
-            this.parent /= gcd;
-        }
-
-        private int gcd() {
-            int a = this.parent;
-            int b = this.child;
-
-            while (b != 0) {
-                int temp = a;
-                a = b;
-                b = temp % b;
-            }
-
-            return a;
-        }
-    }
 }
