261f92c24aac6a02a280bc29fc36c1f3248d5327
ystone17 <ston5519@gmail.com>
2024-01-10 23:30:55 +0900

    2024-01-10
    * x g3 1069_집으로: 기하학
    
    남은 거리가 점프거리 2배 이하이면 무조건 점프 2번으로 목적지에 도달가능하다.
    
    남은 거리를 점프 2번 길이 이하로 한 뒤
    1. 점프 두번 한 시간
    2. 점프 하고 걸어가는 시간
    3. 걸어가는 시간
    
    3가지로 나누어 최소값을 구했지만, 실패

---
+++ gold_3/1069_집으로/src/Main.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int x, y, d, t;
+    static double dist, reminder, res;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+
+        x = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
+        t = Integer.parseInt(st.nextToken());
+
+        reminder = dist = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+
+        while (reminder > d * 2) {
+            res += t;
+            reminder -= d;
+        }
+
+        double doubleJump = t * 2;
+        double jumpAndWalk = t + (Math.abs(d - reminder));
+        double walk = reminder;
+
+        System.out.println(res + Math.min(doubleJump, Math.min(jumpAndWalk, walk)));
+    }
+}
