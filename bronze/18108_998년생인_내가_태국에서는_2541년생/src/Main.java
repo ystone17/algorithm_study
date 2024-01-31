6f2e48c0a83b197ee4e92715f295825c9738ee2d
ystone17 <ston5519@gmail.com>
2024-01-31 22:52:35 +0900

    2024-01-31
    * o b5 18108_998년생인_내가_태국에서는_2541년생:

---
+++ bronze/18108_998년생인_내가_태국에서는_2541년생/src/Main.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(Integer.parseInt(br.readLine()) - 543);
+    }
+}
