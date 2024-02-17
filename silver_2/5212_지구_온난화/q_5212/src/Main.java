f104eccf36d70f6b7d089a9b7177bac2ec4399f0
ystone17 <ston5519@gmail.com>
2024-02-17 23:34:06 +0900

    2024-02-17
    * x s2 5212_지구_온난화: bfs

---
+++ silver_2/5212_지구_온난화/q_5212/src/Main.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int yLen, xLen;
+
+    static int[] dy = {0, 1, 0, -1};
+    static int[] dx = {1, 0, -1, 0};
+
+    public static void main(String[] args) {
+
+
+    }
+}
