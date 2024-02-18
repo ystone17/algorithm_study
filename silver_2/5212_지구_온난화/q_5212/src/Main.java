660cc1050a90aa4bd47480baa8576595d097a402
ystone17 <ston5519@gmail.com>
2024-02-19 02:27:03 +0900

    2024-02-19
    * o s2 5212_지구_온난화: bfs

--- silver_2/5212_지구_온난화/q_5212/src/Main.java
+++ silver_2/5212_지구_온난화/q_5212/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -27,6 +28,9 @@ public class Main {
         }
 
         copyMap = new char[yLen][xLen];
+        for (char[] row : copyMap) {
+            Arrays.fill(row, '.');
+        }
 
         for (int y = 0; y < yLen; y++) {
             for (int x = 0; x < xLen; x++) {
@@ -53,42 +57,27 @@ public class Main {
             }
         }
 
-        var y1 = -1;
-        var x1 = -1;
-        var y2 = -1;
-        var x2 = -1;
+        var minY = 1000;
+        var minX = 1000;
+        var maxY = -1;
+        var maxX = -1;
 
-        for (int x = 0; x < xLen; x++) {
-            for (int y = 0; y < yLen; y++) {
+        for (int y = 0; y < yLen; y++) {
+            for (int x = 0; x < xLen; x++) {
                 if (copyMap[y][x] == 'X') {
-                    y1 = y;
-                    x1 = x;
-                    break;
+                    minY = Math.min(minY, y);
+                    minX = Math.min(minX, x);
+                    maxY = Math.max(maxY, y);
+                    maxX = Math.max(maxX, x);
                 }
-            }
-            if (y1 != -1) {
-                break;
             }
         }
 
-        for (int x = xLen - 1; x >= 0; x--) {
-            for (int y = yLen - 1; y >= 0; y--) {
-                if (copyMap[y][x] == 'X') {
-                    y2 = y;
-                    x2 = x;
-                    break;
-                }
-            }
-            if (y2 != -1) {
-                break;
-            }
-        }
-
-        for (int y = y1; y <= y2; y++) {
-            for (int x = x1; x <= x2; x++) {
+        for (int y = minY; y <= maxY; y++) {
+            for (int x = minX; x <= maxX; x++) {
                 sb.append(copyMap[y][x]);
             }
-            sb.append("\n");
+            sb.append("\n" );
         }
 
         System.out.println(sb);
